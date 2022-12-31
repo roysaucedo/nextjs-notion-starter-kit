f1a6ba9a89f79d6ca30d4dbc859ffcd801928c1c
Roy Saucedo <72239122+roysaucedo@users.noreply.github.com>
2022-12-31 14:47:34 -0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,11 +5,11 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/'
+      href='https://github.com/roysaucedo'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
-      aria-label='View source on GitHub Roy'
+      aria-label='View source on GitHub'
     >
       <svg
         width='80'
