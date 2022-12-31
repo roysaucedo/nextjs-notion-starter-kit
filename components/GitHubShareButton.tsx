88ae190e0197d06b0b55c217717e07a219ce3781
Roy Saucedo <72239122+roysaucedo@users.noreply.github.com>
2022-12-31 14:05:49 -0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -4,6 +4,7 @@ import styles from './styles.module.css'
 
 export const GitHubShareButton: React.FC = () => {
   return (
+    
     <a
       href='https://github.com/transitive-bullshit/nextjs-notion-starter-kit'
       target='_blank'
@@ -11,6 +12,7 @@ export const GitHubShareButton: React.FC = () => {
       className={styles.githubCorner}
       aria-label='View source on GitHub'
     >
+      roy
       <svg
         width='80'
         height='80'
