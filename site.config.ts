73c6a81b63c21a429e0a040f89baafa1f96655cd
Roy Saucedo <72239122+roysaucedo@users.noreply.github.com>
2023-06-19 09:00:50 -0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,7 +17,6 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'royasaucedo',
   github: 'roysaucedo',
   linkedin: 'roysaucedo',
   youtube: 'channel/UCSkkIddd50SReiAR1eP38bQ',
