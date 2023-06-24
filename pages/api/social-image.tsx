5258129fcd85a8556cf0179a7709b07d878d7019
roysaucedo <roy.a.saucedo@gmail.com>
2023-06-24 12:04:17 -0700

    page title

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -118,7 +118,7 @@ export default async function OGImage(req: NextRequest) {
                 fontFamily: 'Inter'
               }}
             >
-              
+              Page Title
             </div>
 
             
