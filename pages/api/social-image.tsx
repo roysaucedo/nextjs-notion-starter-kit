2d7eb5a7ab5fe76377e5d895fc2f05a6cf892497
roysaucedo <roy.a.saucedo@gmail.com>
2023-06-24 11:37:05 -0700

    remove title

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -118,7 +118,7 @@ export default async function OGImage(req: NextRequest) {
                 fontFamily: 'Inter'
               }}
             >
-              {pageInfo.title}
+              
             </div>
 
             
