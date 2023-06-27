38127f8cf8339a05329182e3ebbd449b077a3cde
Roy Saucedo <72239122+roysaucedo@users.noreply.github.com>
2023-06-27 16:49:39 -0700

    Update social-image.tsx - remove

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -84,63 +84,9 @@ export default async function OGImage(req: NextRequest) {
           />
         )}
 
-        <div
-          style={{
-            position: 'relative',
-            width: 900,
-            height: 465,
-            display: 'none',
-            flexDirection: 'column',
-            border: '16px solid rgba(0,0,0,0.3)',
-            borderRadius: 8,
-            zIndex: '1'
-          }}
-        >
-          <div
-            style={{
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'space-around',
-              backgroundColor: '#fff',
-              padding: 24,
-              alignItems: 'center',
-              textAlign: 'center'
-            }}
-          >
-            
+        
 
-            
-
-            
-          </div>
-        </div>
-
-        {pageInfo.authorImage && (
-          <div
-            style={{
-              position: 'absolute',
-              top: 47,
-              left: 104,
-              height: 150,
-              width: 150,
-              display: 'flex',
-              borderRadius: '50%',
-              border: '4px solid #fff',
-              zIndex: '5'
-            }}
-          >
-            <img
-              src={pageInfo.authorImage}
-              style={{
-                width: '100%',
-                height: '100%'
-                // transform: 'scale(1.04)'
-              }}
-            />
-          </div>
-        )}
+        
       </div>
     ),
     {
